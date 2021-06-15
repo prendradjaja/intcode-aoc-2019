7ced17c9f308dde9d63698f7df0b2b6bdc22f76b
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-06-14 19:54:19 -0700

    run-all-tests.sh

--- 05a/main-02b.py
+++ 05a/main-02b.py
@@ -1,15 +1,18 @@
 from puzzle_inputs import day_2_program as program, day_2_target_output as target_output
 from intcode import IntcodeComputer
 
-def main():
+def main(
+    in1s = range(0, 99+1),
+    in2s = range(0, 99+1)
+):
     """
-    >>> main()
+    >>> main([52], [8])  # Don't run on all inputs when testing -- faster to just provide the known answer
     5208
     """
     computer = IntcodeComputer()
 
-    for in1 in range(0, 99+1):
-        for in2 in range(0, 99+1):
+    for in1 in in1s:
+        for in2 in in2s:
             copy = list(program)
             computer.load_memory(copy)
             computer.memory[1] = in1
