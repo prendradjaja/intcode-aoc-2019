cf013f29cc409112789e612b87f7da972f38eccc
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-06-16 16:25:23 -0700

    ++

--- 09/nth-triangle-number.py
+++ 09/nth-triangle-number.py
@@ -1,5 +1,15 @@
 from intcode import IntcodeComputer
 from io_devices import OneValueInput, PrintOutput
+from variables import VariableAllocator
+
+CODE_SEGMENT_LENGTH = 100
+DATA_SEGMENT_LENGTH = 100
+'';                                                                                                 program = [0] * (CODE_SEGMENT_LENGTH + DATA_SEGMENT_LENGTH); va = VariableAllocator(CODE_SEGMENT_LENGTH, DATA_SEGMENT_LENGTH, program)
+
+limit    = va.make_var(4)
+i        = va.make_var(1)
+total    = va.make_var(0)
+loopcond = va.make_var(0)
 
 # n = limit = 4
 # i = 1
@@ -9,47 +19,24 @@ from io_devices import OneValueInput, PrintOutput
 #     i += 1
 # print(total)
 
-CODE_SEGMENT_LENGTH = 100
-DATA_SEGMENT_LENGTH = 100
-
-LOOP = 0
-END = 18
-
-def make_var(initial_value):
-    address = make_var.next
-    program[address] = initial_value
-    make_var.next += 1
-    return address
-make_var.next = DATA_SEGMENT_LENGTH
-
-program = [0] * (CODE_SEGMENT_LENGTH + DATA_SEGMENT_LENGTH)
-
-limit    = make_var(4)
-i        = make_var(1)
-total    = make_var(0)
-loopcond = make_var(0)
-
 code = [
-    # LOOP: (@8)
-    # @103 = i == limit
+];LOOP                                                                                              = len(code); code += [
+    # loopcond = i == limit
     8, limit, i, loopcond,
-    # if @103 jump to END
-    1005, loopcond, END,
+    # if loopcond jump to END
+    1005, loopcond, lambda: END,
 
     # total += i
     1, i, total, total,
     # i += 1
     101, 1, i, i,
     # jump to LOOP
-    1105, 1, LOOP,
-
-    # END:
-    # print(total)
+    1105, 1, lambda: LOOP,
+];END                                                                                               = len(code); code += [
+    # print total
     4, total,
     99,
-]
-
-program[:len(code)] = code
+];                                                                                                  code = [n if isinstance(n, int) else n() for n in code]; assert len(code) <= CODE_SEGMENT_LENGTH; program[:len(code)] = code
 
 computer = IntcodeComputer(None, PrintOutput())
 computer.run(program)
