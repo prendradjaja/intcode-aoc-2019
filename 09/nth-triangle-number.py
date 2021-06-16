450365a1e1e5ef8b001afc4fb0271e44809c51b8
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-06-16 16:05:26 -0700

    ++

--- 09/nth-triangle-number.py
+++ 09/nth-triangle-number.py
@@ -9,38 +9,47 @@ from io_devices import OneValueInput, PrintOutput
 #     i += 1
 # print(total)
 
+CODE_SEGMENT_LENGTH = 100
+DATA_SEGMENT_LENGTH = 100
+
 LOOP = 0
 END = 18
 
-# code segment
-program = [
+def make_var(initial_value):
+    address = make_var.next
+    program[address] = initial_value
+    make_var.next += 1
+    return address
+make_var.next = DATA_SEGMENT_LENGTH
+
+program = [0] * (CODE_SEGMENT_LENGTH + DATA_SEGMENT_LENGTH)
+
+limit    = make_var(4)
+i        = make_var(1)
+total    = make_var(0)
+loopcond = make_var(0)
+
+code = [
     # LOOP: (@8)
     # @103 = i == limit
-    8, 100, 101, 103,
+    8, limit, i, loopcond,
     # if @103 jump to END
-    1005, 103, END,
+    1005, loopcond, END,
 
     # total += i
-    1, 101, 102, 102,
+    1, i, total, total,
     # i += 1
-    101, 1, 101, 101,
+    101, 1, i, i,
     # jump to LOOP
     1105, 1, LOOP,
 
     # END:
     # print(total)
-    4, 102,
+    4, total,
     99,
 ]
 
-# padding and data segment
-program += (
-    [0] * (100 - len(program)) +
-    # n = limit = 4 (@100)
-    # i = 1 (@101)
-    # total = 0 (@102)
-    [4, 1]
-)
+program[:len(code)] = code
 
 computer = IntcodeComputer(None, PrintOutput())
 computer.run(program)
