af5de365558607a3fb035802681a2e9406f37e15
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-06-15 17:29:21 -0700

    Starter code for 07a

--- 07a/main.py
+++ 07a/main.py
@@ -1,33 +1,13 @@
-from puzzle_inputs import day_5_program as program, day_5b_larger_example
+from puzzle_inputs import day_7_program as program
 from intcode import IntcodeComputer
 from io_devices import OneValueInput, PrintOutput
 
 def main():
     """
     >>> main()
-    14340395
     """
-    computer = IntcodeComputer(
-        OneValueInput(5),
-        PrintOutput()
-    )
+    computer = IntcodeComputer()
     computer.load_memory(program)
-    computer.run()
-
-def large_example(n):
-    """
-    >>> large_example(5)
-    999
-    >>> large_example(8)
-    1000
-    >>> large_example(10)
-    1001
-    """
-    computer = IntcodeComputer(
-        OneValueInput(n),
-        PrintOutput()
-    )
-    computer.load_memory(day_5b_larger_example)
     computer.run()
 
 if __name__ == '__main__':
--- 07a/run-all-tests.sh
+++ 07a/run-all-tests.sh
@@ -5,5 +5,6 @@ set -ex
 python3 -m doctest -v main-02a.py
 python3 -m doctest -v main-02b.py
 python3 -m doctest -v main-05a.py
+python3 -m doctest -v main-05b.py
 
-python3 -m doctest -v main.py
+# python3 -m doctest -v main.py
