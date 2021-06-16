54d0f36d8a048f7dc5b4108856296c403084dbc2
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-06-15 21:41:03 -0700

    nth triangular ++

--- 09/nth-triangle-number.py
+++ 09/nth-triangle-number.py
@@ -9,35 +9,38 @@ from io_devices import OneValueInput, PrintOutput
 #     i += 1
 # print(total)
 
-LOOP = 8
-END = 26
+LOOP = 0
+END = 18
 
+# code segment
 program = [
-# n = limit = 4 (@100)
-1101, 4, 0, 100,
-# i = 1 (@101)
-1101, 1, 0, 101,
-# total = 0 (@102)
-*[],
+    # LOOP: (@8)
+    # @103 = i == limit
+    8, 100, 101, 103,
+    # if @103 jump to END
+    1005, 103, END,
 
-# LOOP: (@8)
-# @103 = i == limit
-8, 100, 101, 103,
-# if @103 jump to END
-1005, 103, 26,
+    # total += i
+    1, 101, 102, 102,
+    # i += 1
+    101, 1, 101, 101,
+    # jump to LOOP
+    1105, 1, LOOP,
 
-# total += i
-1, 101, 102, 102,
-# i += 1
-101, 1, 101, 101,
-# jump to LOOP
-1105, 1, 8,
-
-# END:
-# print(total)
-4, 102,
-99,
+    # END:
+    # print(total)
+    4, 102,
+    99,
 ]
+
+# padding and data segment
+program += (
+    [0] * (100 - len(program)) +
+    # n = limit = 4 (@100)
+    # i = 1 (@101)
+    # total = 0 (@102)
+    [4, 1]
+)
 
 computer = IntcodeComputer(None, PrintOutput())
 computer.run(program)
