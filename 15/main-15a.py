dd83c39c3b7c7c87792d908f7fe1ed5e9b3dd103
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-11-21 20:11:34 -0600

    WIP 15b

--- 15/main-15a.py
+++ 15/main-15a.py
@@ -19,7 +19,7 @@ from io_devices import Input, Output
 
 
 def main(_stdscr):
-    global HEIGHT, HALF_WIDTH, stdscr, last_movement_command, droid_position
+    global HEIGHT, HALF_WIDTH, stdscr, last_movement_command, droid_position, maze_map
     stdscr = _stdscr
 
     HEIGHT = curses.LINES
@@ -27,9 +27,11 @@ def main(_stdscr):
 
     last_movement_command = None
     droid_position = SimpleNamespace(r=0, c=0)
+    maze_map = {(0, 0): '.'}
 
     stdscr.clear()
     draw(droid_position, 'D')
+    draw2(SimpleNamespace(r=0, c=0))
 
     computer = IntcodeComputer(
         input_ = CallbackInput(handle_input),
@@ -40,19 +42,47 @@ def main(_stdscr):
 
 def handle_input():
     global last_movement_command
+    # stdscr.getkey()
     last_movement_command = random.randint(1, 4)
     return last_movement_command
+
+
+# def handle_input():
+#     global last_movement_command
+#     while True:
+#         key = stdscr.getkey()
+#         if key == 'KEY_UP':
+#             result = 1
+#             break
+#         elif key == 'KEY_DOWN':
+#             result = 2
+#             break
+#         elif key == 'KEY_LEFT':
+#             result = 3
+#             break
+#         elif key == 'KEY_RIGHT':
+#             result = 4
+#             break
+#     last_movement_command = result
+#     return result
 
 
 def handle_output(output):
     if output == 0:
         wall_position = get_movement_destination(last_movement_command, droid_position)
         draw(wall_position, '#')
+        maze_map[to_tuple(wall_position)] = '#'
+        draw2(wall_position)
+        check_neighbors(wall_position)
     elif output in [1, 2]:
         previous_position = copy(droid_position)
         move_droid(last_movement_command, droid_position)
         draw(previous_position, '.')
         draw(droid_position, 'D')
+        if to_tuple(droid_position) not in maze_map:
+            maze_map[to_tuple(droid_position)] = '.'
+            draw2(droid_position)
+        check_neighbors(droid_position)
         if output == 2:
             stdscr.addstr(1, 0, f'Oxygen system found! Location: {droid_position}')
             draw(SimpleNamespace(r=0, c=0), 'S')
@@ -62,10 +92,61 @@ def handle_output(output):
         raise Exception('Unexpected output')
 
 
+def check_neighbors(position):
+    check(get_movement_destination(1, position))
+    check(get_movement_destination(2, position))
+    check(get_movement_destination(3, position))
+    check(get_movement_destination(4, position))
+
+
+def check(position):
+    neighbors = [
+        get_movement_destination(1, position),
+        get_movement_destination(2, position),
+        get_movement_destination(3, position),
+        get_movement_destination(4, position),
+    ]
+    if maze_map.get(to_tuple(position), None) == '.' and all(to_tuple(neighbor) in maze_map for neighbor in neighbors):
+        maze_map[to_tuple(position)] = 'o'
+        draw2(position)
+
+
+MAX_LINES = 20
+class Logger:
+    def __init__(self):
+        self.lines = ['' * 100] * MAX_LINES
+
+    def log(self, line=''):
+        line = str(line)[:100].ljust(100)
+        self.lines.pop(0)
+        self.lines.append(line)
+        for i, line in enumerate(reversed(['---'] + self.lines)):
+            stdscr.addstr(HEIGHT - i - 1, 0, line)
+        stdscr.addstr(0, 0, '')  # Keep the cursor in the corner
+
+log = Logger().log
+
+
+
+def to_tuple(position):
+    return (position.r, position.c)
+
+
 def draw(position, ch):
     stdscr.addstr(
         position.r + HEIGHT // 2,
-        (position.c * 2) + HALF_WIDTH,
+        (position.c * 2) + HALF_WIDTH // 2,
+        ch
+    )
+    stdscr.addstr(0, 0, '')  # Keep the cursor in the corner
+    stdscr.refresh()
+
+
+def draw2(position):
+    ch = maze_map[to_tuple(position)]
+    stdscr.addstr(
+        position.r + HEIGHT // 2,
+        (position.c * 2) + (3 * HALF_WIDTH // 2),
         ch
     )
     stdscr.addstr(0, 0, '')  # Keep the cursor in the corner
